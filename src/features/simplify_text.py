f3d83542281cc04fb3ce635198d40b7b5f6e708e
chpatola <charlotte.patola@helsinki.fi>
2020-04-11 14:08:30 +0300

    working code

--- src/features/simplify_text.py
+++ src/features/simplify_text.py
@@ -8,7 +8,7 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 #lemmmatization https://antupis.github.io/lemmatization/finnish/2019/06/12/Lemmatizing-finnish-text.html
 from src import Generic_func as gf
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer#bag of words
 from collections import Counter
 import scattertext as st
 import spacy
@@ -28,6 +28,25 @@ def prepare_sentences(text, stop ="no"):
     newtext  = " ".join([stemmer.stem(word) for word in newtext ]) #As is it last step, we join the words back to a sentence
     return newtext
 
+def wordfreqdf(df):
+    corpus = st.CorpusFromPandas(yle_data, category_col='party', text_col='work_for', nlp=nlp).build()
+    term_freq_df = corpus.get_term_freq_df()
+
+    result_df = pd.DataFrame(columns=["1","2","3","4","5","6","7","8","9","10"])
+    parties= df['party'].sort_values().unique()
+
+    index =0
+    for party in parties:
+        party_score_name = party + "_Score"
+        term_freq_df[party_score_name] = corpus.get_scaled_f_scores(party)
+        result_df.loc[index] = list(term_freq_df.sort_values(by=party_score_name, ascending=False).index[:10])
+        index = index +1
+    
+    result_df["Party"] = parties
+    result_df.set_index("Party",inplace=True)
+
+    return result_df  
+
 #1. Import data and correct scandinavian characters
 path = '/home/chpatola/Desktop/Skola/Python/cookie_nlp/data/interim/cleaned_data.csv'
 yle_data = pd.read_csv(path, sep=',', encoding="ISO-8859-1")
@@ -40,39 +59,7 @@ yle_data.head(5)
 # 3. Write processed data to folder
 yle_data.to_csv("/home/chpatola/Desktop/Skola/Python/cookie_nlp/data/processed/processed_data.csv",index=False,encoding="ISO-8859-1")
 
+#4. Get insights into 10 most typical words for each party
+party_word_freq = wordfreqdf(yle_data)
+party_word_freq.to_csv('/home/chpatola/Desktop/Skola/Python/cookie_nlp/reports/party_word_freq.csv',sep=',', encoding="ISO-8859-1")
 
-
-#4. Get insights into 10 most common word for each party
-corpus = st.CorpusFromPandas(yle_data, category_col='party', text_col='work_for', nlp=nlp).build()
-term_freq_df = corpus.get_term_freq_df()
-
-term_freq_df['Kokoomus_score'] = corpus.get_scaled_f_scores('Kansallinen Kokoomus')
-print(list(term_freq_df.sort_values(by='Kokoomus_score', ascending=False).index[:10]))
-term_freq_df['Perus_score'] = corpus.get_scaled_f_scores('Perussuomalaiset')
-print(list(term_freq_df.sort_values(by='Perus_score', ascending=False).index[:10]))
-term_freq_df['Keskusta_score'] = corpus.get_scaled_f_scores('Suomen Keskusta')
-print(list(term_freq_df.sort_values(by='Keskusta_score', ascending=False).index[:10]))
-term_freq_df['Krist_score'] = corpus.get_scaled_f_scores('Suomen Kristillisdemokraatit (KD)')
-print(list(term_freq_df.sort_values(by='Krist_score', ascending=False).index[:10]))
-term_freq_df['SDP_score'] = corpus.get_scaled_f_scores('Suomen Sosialidemokraattinen Puolue')
-print(list(term_freq_df.sort_values(by='SDP_score', ascending=False).index[:10]))
-term_freq_df['Vasen_score'] = corpus.get_scaled_f_scores('Vasemmistoliitto')
-print(list(term_freq_df.sort_values(by='Vasen_score', ascending=False).index[:10]))
-term_freq_df['Vihreä_score'] = corpus.get_scaled_f_scores('Vihreä liitto')
-print(list(term_freq_df.sort_values(by='Vihreä_score', ascending=False).index[:10]))
-
-
-
-
-yle_data_explore["work_for"] = yle_data_explore["work_for"].apply(lambda x : x.lower())
-yle_data_explore["work_for"] = yle_data_explore["work_for"].apply(lambda x : "".join([sign for sign in x if sign not in string.punctuation]))  
-#yle_data_explore["work_for"] = yle_data_explore["work_for"].apply(lambda x : "".join([sign for sign in x if sign not in stopwords.words("finnish")])) 
-yle_data_explore.head(4)
-yle_data_explore["work_for"][0][0]
-
-kokoomus_most_common = Counter(" ".join(yle_data_explore[yle_data_explore["party"]=="Kansallinen Kokoomus"].work_for.dropna()).split()).most_common(10)
-print(kokoomus_most_common)
-perus_most_common = Counter(" ".join(yle_data_explore[yle_data_explore["party"]=="Perussuomalaiset"].work_for.dropna()).split()).most_common(10)
-print(perus_most_common)
-keskusta_most_common = Counter(" ".join(yle_data_explore[yle_data_explore["party"]=="Suomen Keskusta"].work_for.dropna()).split()).most_common(10)
-print(keskusta_most_common)
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -13,6 +13,10 @@ from sklearn.metrics import confusion_matrix
 from src.models import train_model
 from lime import lime_text
 from lime.lime_text import LimeTextExplainer
+import matplotlib.pyplot as plt
+import itertools
+from src.visualization import Visualize
+import seaborn as sns
 
 #1. Test model and look into results
 val_X, val_y, bag, tf_idf = train_model.train_model()
@@ -22,15 +26,14 @@ print("Predictions:\n {} \nTruth:\n {}".format(predictions[0:3],val_y[0:3]))
 print(val_X[0:3])
 
 print("Mean accurancy: {:.2f} %".format(100*bag.best_score_))
-print(classification_report(val_y,predictions))
-print("Row is truth, columns guess\n {}".format(confusion_matrix(val_y,predictions)))
 
-explainer = LimeTextExplainer(class_names=np.unique(val_y))
-exp = explainer.explain_instance(val_X[0], bag.predict_proba, num_features=6, labels=[0, 1])
-print ('Explanation for class %s' % np.unique(val_y)[0])
-print ('\n'.join(map(str, exp.as_list(label=0))))
-exp.show_in_notebook(text=False)
-exp.show_in_notebook(text=val_X[0], labels=(0,))
-#% chans for each party for a specific row
-print("Probability for text to belong to each part:\n{}".format(bag.predict_proba([val_X[0]]).round(3)*100))
+#2. Save classification report and confusion matrix to file
+classi_rep = _plot_classification_report(val_y,predictions)
+classi_rep.savefig('/home/chpatola/Desktop/Skola/Python/cookie_nlp/reports/figures/classificationReport.png',bbox_inches='tight')
+
+parties= val_y.sort_values().unique()
+cm_analysis(val_y,predictions,'/home/chpatola/Desktop/Skola/Python/cookie_nlp/reports/figures/confusion_matrix.png',labels=parties)
+
+
+
 
